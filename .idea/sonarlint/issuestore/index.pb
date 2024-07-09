1b4b0924e4919ea3bb8e9408c25d753bb8709d33
SergeyLukash1807 <sergeylukash180796@gmail.com>
2024-07-09 17:00:59 +0200

    Lesson_16

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,16 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+X
+(src/test/java/org/example/MtsbyTest.java,b\5\b5e095d44611f0442709110831715daa32450b7e
+Å
+Q.idea/sonarlint/securityhotspotstore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,2\c\2cabb939da04d3de3a200bf2b575690e1a16316d
+Å
+Q.idea/sonarlint/securityhotspotstore/b/5/b5e095d44611f0442709110831715daa32450b7e,b\9\b9a8cc4b850d530535e5c8c717cd09d6f57716e8
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+R
+"src/main/java/org/example/App.java,2\6\268b09d2a5cdab7d148f0ec91f7b3fb51601410d
+w
+G.idea/sonarlint/issuestore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,8\6\863e0292fd7e9b3e74dafd6cc01f73ba76346482
---
+++ src/test/java/org/example/MtsbyTest.java
@@ -0,0 +1,176 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MtsbyTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver-win64/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://mts.by");
+        closeCookieBanner();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    private void closeCookieBanner() {
+        try {
+            WebElement cookieBanner = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body > div.layout.layout--default > main > div > div.cookie.show > div > div.cookie__buttons > button.btn.btn_gray.cookie__cancel")));
+            cookieBanner.click();
+        } catch (Exception e) {
+            System.out.println("Cookie banner not found or not visible.");
+        }
+    }
+
+    @Test
+    public void testFieldTextForAllPaymentOptions() {
+
+        checkFieldText("Услуги связи", "Номер телефона", "Сумма", "E-mail для отправки чека");
+        checkFieldText("Домашний интернет", "Номер абонента", "Сумма", "E-mail для отправки чека");
+        checkFieldText("Рассрочка", "Номер счета на 44", "Сумма", "E-mail для отправки чека");
+        checkFieldText("Задолженность", "Номер счета на 2073", "Сумма", "E-mail для отправки чека");
+    }
+
+
+    private void checkFieldText(String mainService, String expectedSecondPlaceholder, String expectedSumPlaceholder, String expectedEmailPlaceholder) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        WebElement dropdownButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".select__header")));
+        dropdownButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".select__list")));
+
+
+        List<WebElement> listItems = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".select__item .select__option")));
+
+        for (WebElement item : listItems) {
+            if (mainService.equals(item.getText())) {
+                item.click();
+                break;
+            }
+        }
+
+        String secondPlaceholder = null;
+        String sumPlaceholder = null;
+        String emailPlaceholder = null;
+
+        switch (mainService) {
+            case "Услуги связи":
+                secondPlaceholder = "#connection-phone";
+                sumPlaceholder = "#connection-sum";
+                emailPlaceholder = "#connection-email";
+                break;
+            case "Домашний интернет":
+                secondPlaceholder = "#internet-phone";
+                sumPlaceholder = "#internet-sum";
+                emailPlaceholder = "#internet-email";
+                break;
+            case "Рассрочка":
+                secondPlaceholder = "#score-instalment";
+                sumPlaceholder = "#instalment-sum";
+                emailPlaceholder = "#instalment-email";
+                break;
+            case "Задолженность":
+                secondPlaceholder = "#score-arrears";
+                sumPlaceholder = "#arrears-sum";
+                emailPlaceholder = "#arrears-email";
+                break;
+        }
+
+
+        WebElement secondFieldInFrame = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(secondPlaceholder)));
+        String actualSecondPlaceholder = secondFieldInFrame.getAttribute("placeholder");
+        assertEquals(expectedSecondPlaceholder, actualSecondPlaceholder);
+
+        WebElement sumFieldInFrame = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(sumPlaceholder)));
+        String actualSumPlaceholder = sumFieldInFrame.getAttribute("placeholder");
+        assertEquals(expectedSumPlaceholder, actualSumPlaceholder);
+
+        WebElement emailFieldInFrame = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(emailPlaceholder)));
+        String actualEmailPlaceholder = emailFieldInFrame.getAttribute("placeholder");
+        assertEquals(expectedEmailPlaceholder, actualEmailPlaceholder);
+
+    }
+
+
+    @Test
+    public void testServicePaymentProcess () {
+        WebElement dropdownButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".select__header")));
+        dropdownButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".select__list")));
+
+
+        List<WebElement> listItems = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".select__item .select__option")));
+
+        for (WebElement item : listItems) {
+            if ("Услуги связи".equals(item.getText())) {
+                    item.click();
+                    break;
+            }
+        }
+        processPaymentForServices("297777777", "100");
+    }
+
+
+    private void processPaymentForServices (String phone, String amount){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        WebElement phoneField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#connection-phone")));
+        phoneField.sendKeys(phone);
+
+        WebElement sumField = driver.findElement(By.cssSelector("#connection-sum"));
+        sumField.sendKeys(amount);
+
+        WebElement continueButton = driver.findElement(By.cssSelector("#pay-connection > button"));
+        assertTrue(continueButton.isEnabled());
+        continueButton.click();
+
+
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector("body > div.bepaid-app > div > iframe")));
+
+        WebElement amountField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body > app-root > div > div > div > app-payment-container > section > div > div > div.pay-description__cost > span:nth-child(1)")));
+        assertEquals(amount + ".00 BYN", amountField.getText());
+        WebElement amountFieldButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/app-root/div/div/div/app-payment-container/section/div/app-card-page/div/div[1]/button")));
+        assertEquals("Оплатить " + amount + ".00 BYN", amountFieldButton.getText());
+        WebElement phoneFieldInFrame = driver.findElement(By.xpath("//span[contains(text(), '" + phone + "')]"));
+        assertEquals("Оплата: Услуги связи Номер:375" + phone, phoneFieldInFrame.getText());
+
+        WebElement cardNumberField = driver.findElement(By.xpath("/html/body/app-root/div/div/div/app-payment-container/section/div/app-card-page/div/div[1]/app-card-input/form/div[1]/div[1]/app-input/div/div/div[1]"));
+        assertTrue(cardNumberField.getText().contains("Номер карты"));
+
+        WebElement cardExpiryField = driver.findElement(By.cssSelector("body > app-root > div > div > div > app-payment-container > section > div > app-card-page > div > div.card-page__card > app-card-input > form > div.card.ng-tns-c61-0 > div:nth-child(2) > div.expires-input.ng-tns-c61-0.ng-star-inserted > app-input > div > div > div.content.ng-tns-c46-4 > label"));
+        assertTrue(cardExpiryField.getText().contains("Срок действия"));
+
+        WebElement cardCvvField = driver.findElement(By.cssSelector("body > app-root > div > div > div > app-payment-container > section > div > app-card-page > div > div.card-page__card > app-card-input > form > div.card.ng-tns-c61-0 > div:nth-child(2) > div.cvc-input.ng-tns-c61-0.ng-star-inserted > app-input > div > div > div.content.ng-tns-c46-5 > label"));
+        assertTrue(cardCvvField.getText().contains("CVC"));
+
+        WebElement paymentIcons = driver.findElement(By.cssSelector("body > app-root > div > div > div > app-payment-container > section > div > app-card-page > div > div.card-page__card > app-card-input > form > div.card.ng-tns-c61-0 > div:nth-child(1) > app-input > div > div > div.icons-container.ng-tns-c46-1 > div > div"));
+        assertTrue(paymentIcons.isDisplayed());
+
+        driver.switchTo().defaultContent();
+    }
+}
+
+
